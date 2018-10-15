00bc5877eeafa27672c8d54766383a163a755d45
suresh <suresh@MININT-CEHQK6N.northamerica.corp.microsoft.com>
2018-10-15 11:49:12 +0530

    Updates

---
+++ precache-manifest.73e478df0f4d35f6df32d8657eb685d4.js
@@ -0,0 +1,238 @@
+self.__precacheManifest = [
+  {
+    "revision": "6eeb746d6f8ef68d6787",
+    "url": "/outlook-web-addin/static/js/28.6eeb746d.chunk.js"
+  },
+  {
+    "revision": "30dcfb4b4d1d2a8ae91e",
+    "url": "/outlook-web-addin/static/js/0.30dcfb4b.chunk.js"
+  },
+  {
+    "revision": "ec664c2106682ae6aa4d",
+    "url": "/outlook-web-addin/static/js/main.ec664c21.chunk.js"
+  },
+  {
+    "revision": "3a2e6770528e4592f58a",
+    "url": "/outlook-web-addin/static/js/2.3a2e6770.chunk.js"
+  },
+  {
+    "revision": "03d9ba4cf4628b1df5d6",
+    "url": "/outlook-web-addin/static/js/3.03d9ba4c.chunk.js"
+  },
+  {
+    "revision": "6ad3cc7e593dcb5c4280",
+    "url": "/outlook-web-addin/static/js/4.6ad3cc7e.chunk.js"
+  },
+  {
+    "revision": "a4aa90cb4a24ec0b078d",
+    "url": "/outlook-web-addin/static/js/5.a4aa90cb.chunk.js"
+  },
+  {
+    "revision": "0d6dfc8b88d07f484df7",
+    "url": "/outlook-web-addin/static/js/6.0d6dfc8b.chunk.js"
+  },
+  {
+    "revision": "5afcf1bfda2cbe02fd15",
+    "url": "/outlook-web-addin/static/js/7.5afcf1bf.chunk.js"
+  },
+  {
+    "revision": "7ae92fbe842103015202",
+    "url": "/outlook-web-addin/static/js/8.7ae92fbe.chunk.js"
+  },
+  {
+    "revision": "8353818695555f3b317d",
+    "url": "/outlook-web-addin/static/js/9.83538186.chunk.js"
+  },
+  {
+    "revision": "6cab9034da4a409bf8ac",
+    "url": "/outlook-web-addin/static/js/10.6cab9034.chunk.js"
+  },
+  {
+    "revision": "03cd4b2ef8d4d8b75347",
+    "url": "/outlook-web-addin/static/js/11.03cd4b2e.chunk.js"
+  },
+  {
+    "revision": "b2fc88d03a8ec1e0cea2",
+    "url": "/outlook-web-addin/static/js/12.b2fc88d0.chunk.js"
+  },
+  {
+    "revision": "dc25c24403668af14517",
+    "url": "/outlook-web-addin/static/js/13.dc25c244.chunk.js"
+  },
+  {
+    "revision": "f6d1a0c62566aad3d77a",
+    "url": "/outlook-web-addin/static/js/14.f6d1a0c6.chunk.js"
+  },
+  {
+    "revision": "dfa8c1fe0c0b624cd106",
+    "url": "/outlook-web-addin/static/js/15.dfa8c1fe.chunk.js"
+  },
+  {
+    "revision": "cea0a90407843c1e1b7a",
+    "url": "/outlook-web-addin/static/js/16.cea0a904.chunk.js"
+  },
+  {
+    "revision": "3832b1704608d2c6ddce",
+    "url": "/outlook-web-addin/static/js/17.3832b170.chunk.js"
+  },
+  {
+    "revision": "c9233234ac47ca8b9a86",
+    "url": "/outlook-web-addin/static/js/18.c9233234.chunk.js"
+  },
+  {
+    "revision": "67fd3a0dc932ae914f74",
+    "url": "/outlook-web-addin/static/js/19.67fd3a0d.chunk.js"
+  },
+  {
+    "revision": "109b27315c0671156992",
+    "url": "/outlook-web-addin/static/js/20.109b2731.chunk.js"
+  },
+  {
+    "revision": "857f104009ac18889744",
+    "url": "/outlook-web-addin/static/js/21.857f1040.chunk.js"
+  },
+  {
+    "revision": "a6dccf7289a148ead55d",
+    "url": "/outlook-web-addin/static/js/22.a6dccf72.chunk.js"
+  },
+  {
+    "revision": "aa87e963aa108ba65299",
+    "url": "/outlook-web-addin/static/js/23.aa87e963.chunk.js"
+  },
+  {
+    "revision": "6aa41dc071ce6bb3d4eb",
+    "url": "/outlook-web-addin/static/js/24.6aa41dc0.chunk.js"
+  },
+  {
+    "revision": "6cef7ea5855a28c82718",
+    "url": "/outlook-web-addin/static/js/25.6cef7ea5.chunk.js"
+  },
+  {
+    "revision": "5d5274652f64496b491b",
+    "url": "/outlook-web-addin/static/js/26.5d527465.chunk.js"
+  },
+  {
+    "revision": "eea117ef8fc23e807f38",
+    "url": "/outlook-web-addin/static/js/27.eea117ef.chunk.js"
+  },
+  {
+    "revision": "5f911b5a10dd82825b76",
+    "url": "/outlook-web-addin/static/js/runtime~main.5f911b5a.js"
+  },
+  {
+    "revision": "01c04f4d8af73ccda55f",
+    "url": "/outlook-web-addin/static/js/29.01c04f4d.chunk.js"
+  },
+  {
+    "revision": "801399832ba3e6556a87",
+    "url": "/outlook-web-addin/static/js/30.80139983.chunk.js"
+  },
+  {
+    "revision": "8977e6cc732a3bbedb8f",
+    "url": "/outlook-web-addin/static/js/31.8977e6cc.chunk.js"
+  },
+  {
+    "revision": "c68f828cfbf4c0c12af6",
+    "url": "/outlook-web-addin/static/js/32.c68f828c.chunk.js"
+  },
+  {
+    "revision": "00fe133d8ce32a8069a6",
+    "url": "/outlook-web-addin/static/js/33.00fe133d.chunk.js"
+  },
+  {
+    "revision": "a743e105eae95f1e8e84",
+    "url": "/outlook-web-addin/static/js/34.a743e105.chunk.js"
+  },
+  {
+    "revision": "13efb16c7eacd4566e8c",
+    "url": "/outlook-web-addin/static/js/35.13efb16c.chunk.js"
+  },
+  {
+    "revision": "e40faacdeb146eaa8020",
+    "url": "/outlook-web-addin/static/js/36.e40faacd.chunk.js"
+  },
+  {
+    "revision": "9d91fce7d4593f3ae165",
+    "url": "/outlook-web-addin/static/js/37.9d91fce7.chunk.js"
+  },
+  {
+    "revision": "f00e705023889f5e878b",
+    "url": "/outlook-web-addin/static/js/38.f00e7050.chunk.js"
+  },
+  {
+    "revision": "4d0e27345163681dff35",
+    "url": "/outlook-web-addin/static/js/39.4d0e2734.chunk.js"
+  },
+  {
+    "revision": "a19e8f50f3aabc2fb8a4",
+    "url": "/outlook-web-addin/static/js/40.a19e8f50.chunk.js"
+  },
+  {
+    "revision": "4a9dd8c89f69441d1fed",
+    "url": "/outlook-web-addin/static/js/41.4a9dd8c8.chunk.js"
+  },
+  {
+    "revision": "8c5b564fd372640d5235",
+    "url": "/outlook-web-addin/static/js/42.8c5b564f.chunk.js"
+  },
+  {
+    "revision": "1767f6192c30fc32309a",
+    "url": "/outlook-web-addin/static/js/43.1767f619.chunk.js"
+  },
+  {
+    "revision": "1598d2c5b378a30f8aa2",
+    "url": "/outlook-web-addin/static/js/44.1598d2c5.chunk.js"
+  },
+  {
+    "revision": "29e8a4c4fe5e719c52e2",
+    "url": "/outlook-web-addin/static/js/45.29e8a4c4.chunk.js"
+  },
+  {
+    "revision": "b82a6efb7a80dd484eb1",
+    "url": "/outlook-web-addin/static/js/46.b82a6efb.chunk.js"
+  },
+  {
+    "revision": "2476a6207348ff980048",
+    "url": "/outlook-web-addin/static/js/47.2476a620.chunk.js"
+  },
+  {
+    "revision": "8c2b503c5aa001f5e07c",
+    "url": "/outlook-web-addin/static/js/48.8c2b503c.chunk.js"
+  },
+  {
+    "revision": "92081721dafe434a78c6",
+    "url": "/outlook-web-addin/static/js/49.92081721.chunk.js"
+  },
+  {
+    "revision": "bf9c26fee8c9613174c9",
+    "url": "/outlook-web-addin/static/js/54.bf9c26fe.chunk.js"
+  },
+  {
+    "revision": "88f59b49d34332667b0f",
+    "url": "/outlook-web-addin/static/js/50.88f59b49.chunk.js"
+  },
+  {
+    "revision": "25146c40c3107dcb3b1a",
+    "url": "/outlook-web-addin/static/js/51.25146c40.chunk.js"
+  },
+  {
+    "revision": "49e8c8c4ff526b46ec6c",
+    "url": "/outlook-web-addin/static/js/52.49e8c8c4.chunk.js"
+  },
+  {
+    "revision": "7569e3aa8dc806e58a88",
+    "url": "/outlook-web-addin/static/js/53.7569e3aa.chunk.js"
+  },
+  {
+    "revision": "ec664c2106682ae6aa4d",
+    "url": "/outlook-web-addin/static/css/main.5696cb01.chunk.css"
+  },
+  {
+    "revision": "88f59b49d34332667b0f",
+    "url": "/outlook-web-addin/static/css/50.e3f12430.chunk.css"
+  },
+  {
+    "revision": "6265b015905c156ddcea8ccc60fbd6c4",
+    "url": "/outlook-web-addin/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/outlook-web-addin/precache-manifest.73e478df0f4d35f6df32d8657eb685d4.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/outlook-web-addin/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
